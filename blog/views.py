145bf375d5a7b997679dc7b0a47226267d2593c8
Henry Canastuj <henryjc97@gmail.com>
2020-10-05 13:12:20 -0600

    Se agrego la lista de borrador, y autenticacion

--- blog/views.py
+++ blog/views.py
@@ -3,29 +3,33 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion')
+    posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_creacion')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.autor = request.user
-            post.fecha_publicacion = timezone.now()
+        
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -33,10 +37,25 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.autor = request.user
-            post.fecha_publicacion = timezone.now()
+            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publicar()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
     
